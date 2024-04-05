6682862e4ffb2ec6298d9224cda3f1276118bc26
nw1n <your@example.com>
2024-04-05 09:16:06 +0200

    clean

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 import { myName } from './lib/config'
 
 const app = express()
-const port = 3000
+const port = 5000
 
 app.get('/', (req, res) => {
     res.send('Hello, TypeScript with Express!')
